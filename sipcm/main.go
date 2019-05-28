5d0458830c1c4daaa1e8eff167b75d2c3d45f148
Andrei Pelinescu-Onciul <andrei@iptel.org>
2019-05-28 17:25:25 +0200

    main: updated to use the config checker and event ring changes

--- sipcm/main.go
+++ sipcm/main.go
@@ -18,7 +18,7 @@ import (
 	"andrei/sipcallmon"
 )
 
-import _ "net/http/pprof"
+//import _ "net/http/pprof"
 
 func main() {
 	cfg, err := sipcallmon.CfgFromOSArgs(&sipcallmon.DefaultConfig)
@@ -26,5 +26,10 @@ func main() {
 		fmt.Printf("command line arguments error %s\n", err)
 		os.Exit(-1)
 	}
+	if err = sipcallmon.CfgCheck(&cfg); err != nil {
+		fmt.Printf("config  error %s\n", err)
+		os.Exit(-1)
+	}
+	sipcallmon.EventsRing.Init(10240)
 	sipcallmon.Run(&cfg)
 }

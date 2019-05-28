1bb2f44374ed9e1db84efcd990ac8281daf1897f
Andrei Pelinescu-Onciul <andrei@iptel.org>
2019-05-28 17:20:16 +0200

    event_ring: more concurrent read/write support + new access methods
    - support more concurrent access for read & write
    - methods for getting an event with a specified index (if it's out
    of range/already overwritten nil will be returned)

--- events_ring.go
+++ events_ring.go
@@ -3,6 +3,7 @@ package sipcallmon
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"regexp"
 	"strconv"
 	"sync"
@@ -34,31 +35,37 @@ const (
 	EvFilterLast
 )
 
-type evring struct {
+type evState struct {
+	readOnly int
+	invalid  bool
+}
+
+type EvRing struct {
 	lock   sync.Mutex
 	evBlst calltr.EventFlags // blacklist for even types
 	idx    int
 	events []calltr.EventData
-	busy   []bool // marks in-use events
+	state  []evState // marks in-use events
+	newEv  chan struct{}
 }
 
-func (er *evring) Ignore(events ...calltr.EventType) {
+func (er *EvRing) Ignore(events ...calltr.EventType) {
 	for _, ev := range events {
 		er.evBlst.Set(ev)
 	}
 }
 
-func (er *evring) UnIgnore(events ...calltr.EventType) {
+func (er *EvRing) UnIgnore(events ...calltr.EventType) {
 	for _, ev := range events {
 		er.evBlst.Clear(ev)
 	}
 }
 
-func (er *evring) ResetBlst() {
+func (er *EvRing) ResetBlst() {
 	er.evBlst.ResetAll()
 }
 
-func (er *evring) AddUnsafe(ev *calltr.EventData) bool {
+func (er *EvRing) AddUnsafe(ev *calltr.EventData) bool {
 	if len(er.events) == 0 {
 		return false
 	}
@@ -66,68 +73,133 @@ func (er *evring) AddUnsafe(ev *calltr.EventData) bool {
 	start := er.idx
 	for {
 		i = er.idx % len(er.events)
-		if er.busy[i] {
-			fmt.Printf("AddUnsafe: busy %d idx %d start %d\n", i, er.idx, start)
+		er.state[i].invalid = true
+		if er.state[i].readOnly > 0 {
+			fmt.Printf("DBG: AddUnsafe: read-only %d idx %d start %d\n", i, er.idx, start)
 			er.idx++
 			if er.idx-start > len(er.events) {
-				fmt.Printf("AddUnsafe: FAILURE busy %d idx %d start %d\n", i, er.idx, start)
+				fmt.Printf("DBG: AddUnsafe: FAILURE busy %d idx %d start %d\n", i, er.idx, start)
 				return false // all busy
 			}
 			continue
 		}
 		break
 	}
+	//  TODO: unlock here
 	er.events[i].Reset()
 	if er.events[i].Copy(ev) {
+		//  TODO: atomic op
+		er.state[i].invalid = false
 		er.idx++
 		return true
 	}
 	return false
 }
 
-func (er *evring) Add(ev *calltr.EventData) bool {
+func (er *EvRing) Add(ev *calltr.EventData) bool {
 	if er.evBlst.Test(ev.Type) {
 		return true // no failure, we just ignore it
 	}
 	er.lock.Lock()
-	defer er.lock.Unlock()
-	return er.AddUnsafe(ev)
+	ret := er.AddUnsafe(ev)
+	idx := er.idx
+	er.lock.Unlock()
+	if er.newEv != nil {
+		select {
+		case er.newEv <- struct{}{}:
+			// do nothing
+		default:
+			fmt.Fprintf(os.Stderr, "WARNING: send channel full for [%d] %p, skipping signal ...\n", idx, ev)
+		}
+	}
+	return ret
 }
 
-func (er *evring) Iterate(f func(int, int, *calltr.EventData) bool) {
+// FIXME: overflow error
+func (ev *EvRing) Get(pos int) *calltr.EventData {
+	i := pos % len(ev.events)
+	ev.lock.Lock()
+	if pos >= ev.idx || (ev.idx-pos) > len(ev.events) || ev.state[i].invalid {
+		ev.lock.Unlock()
+		return nil
+	}
+	ev.state[i].readOnly++
+	ev.lock.Unlock()
+	return &ev.events[i]
+}
+
+func (ev *EvRing) Put(pos int) {
+	i := pos % len(ev.events)
+	ev.lock.Lock()
+	ev.state[i].readOnly--
+	ev.lock.Unlock()
+	if ev.state[pos%len(ev.events)].readOnly < 0 {
+		panic("Put: below 0")
+	}
+}
+
+func (ev *EvRing) LastIdx() int {
+	return ev.idx
+}
+
+func (ev *EvRing) BufSize() int {
+	return len(ev.events)
+}
+
+// Iterate returns the numnber of elements on which it did iterate
+func (er *EvRing) Iterate(f func(int, int, *calltr.EventData) bool) int {
+	n := 0
 	er.lock.Lock()
 again:
 	start := 0
 	if er.idx > len(er.events) {
 		start = er.idx - len(er.events)
 	}
-	for i := start; i < er.idx; i++ {
-		er.busy[i%len(er.events)] = true
+	for n := start; n < er.idx; n++ {
+		p := n % len(er.events)
+		if er.state[p].invalid {
+			continue // skip
+		}
+
+		er.state[p].readOnly++
 		er.lock.Unlock()
-		cont := f(i, i-start, &er.events[i%len(er.events)])
+		n++
+		cont := f(n, n-start, &er.events[p])
 		er.lock.Lock()
-		er.busy[i%len(er.events)] = false
+		er.state[p].readOnly--
 		if !cont {
 			break
 		}
+		// TODO: start !=0 not needed
 		if start != 0 && (er.idx-len(er.events)) > start {
-			// more stuff added in the meantime
+			// wraparround: too much stuff added in the meantime
 			// re-init
 			fmt.Printf("Iterate: changed e.idx = %d start =%d\n", er.idx, start)
 			goto again
 		}
 	}
 	er.lock.Unlock()
+	return n
 }
 
-var eventsRing evring
+var EventsRing EvRing
 
-func init() {
+func (ev *EvRing) Init(no int) {
 
-	eventsRing.events = make([]calltr.EventData, 102400)
-	eventsRing.busy = make([]bool, len(eventsRing.events))
-	for i := 0; i < len(eventsRing.events); i++ {
-		eventsRing.events[i].Init(make([]byte, calltr.EventDataMaxBuf()))
+	ev.events = make([]calltr.EventData, no)
+	ev.state = make([]evState, len(ev.events))
+	for i := 0; i < len(ev.events); i++ {
+		ev.events[i].Init(make([]byte, calltr.EventDataMaxBuf()))
+	}
+}
+
+func (ev *EvRing) SetEvSignal(ch chan struct{}) {
+	ev.newEv = ch
+}
+func (ev *EvRing) CloseEvSignal() {
+	if ev.newEv != nil {
+		close(ev.newEv)
+		ev.newEv = nil
 	}
 }
 
